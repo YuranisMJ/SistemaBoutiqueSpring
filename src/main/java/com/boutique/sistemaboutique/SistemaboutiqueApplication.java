322f9b1d0233beace298d21012bb1dac5e50a587
Yuranis Martínez Jiménez <jimenezyuranis83@gmail.com>
2026-09-10 22:00:46 -0500

    Configura proyecto Spring Boot para SistemaBoutique

---
+++ src/main/java/com/boutique/sistemaboutique/SistemaboutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.boutique.sistemaboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaboutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaboutiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boutique/sistemaboutique/SistemaboutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutique.sistemaboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaboutiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
